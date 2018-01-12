361198abdd27ba46e255a7bdf5cc196137bbdf27
Yosif Al-Khlefah <435102878@student.ksu.edu.sa>
2018-01-13 01:01:37 +0300

    gg

--- src/App.js
+++ src/App.js
@@ -1,36 +1,3 @@
-// import { AsyncStorage } from 'react-native';
-// import Home from './components/Home';
-// import LoginForm from './components/LoginForm';
-// import { Spinner } from './components/common';
-//
-// class App extends Component {
-//   state = { loggedIn: null }
-//
-//   componentDidMount() {
-//     const value = AsyncStorage.getItem('id')._55;
-//     if (value !== null) {
-//       this.setState({ loggedIn: true });
-//     } else {
-//       this.setState({ loggedIn: false });
-//     }
-//   }
-//   renderContent() {
-//     switch (this.state.loggedIn) {
-//       case true:
-//         return <Home />;
-//       case false:
-//         return <LoginForm />;
-//       default:
-//         return <Spinner size='large' />;
-//     }
-//   }
-//
-//   render() {
-//     return (this.renderContent());
-//   }
-// }
-//
-// export default App;
 import React from 'react';
 import { StackNavigator } from 'react-navigation';
 import Home from './components/Home';
