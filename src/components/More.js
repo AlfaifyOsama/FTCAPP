05094be9666bade953aa04cb8cdd4379983459cc
Yosif Al-Khlefah <435102878@student.ksu.edu.sa>
2018-01-14 00:49:47 +0300

    added nore pages

--- src/components/More.js
+++ src/components/More.js
@@ -1,34 +1,50 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import { List, ListItem } from 'react-native-elements';
 
 
 export default class Home extends Component {
   state = { options: [] } ;
 
+  constructor(props) {
+    super(props);
+
+  }
+
     componentDidMount() {
       this.setState({
         options: [
           {
-            title: 'Appointments',
-            icon: 'av-timer'
+            title: 'رصد النقاط',
+            icon: 'work'
           },
           {
-            title: 'Trips',
-            icon: 'flight-takeoff'
+            title: 'ارسال التنبيهات',
+            icon: 'send',
           }
          ]
     });
   }
+  onPress = (x) => {
+    if(x == 0)
+      this.props.navigation.navigate('ApprovePoints');
+    else if(x == 1)
+      this.props.navigation.navigate('SendNotifications');
 
+
+  }
    
   render() {
+
+    const { pageStyle, listStyle, listItem } = styles;
     return (
-       <View >
-      <List>
+       <View style={pageStyle}>
+      <List style={listStyle}>
   {
     this.state.options.map((item, i) => (
-      <ListItem
+      <ListItem   
+        onPress={() => this.onPress(i)}
+        style={listItem}
         key={i}
         title={item.title}
         leftIcon={{name: item.icon}}
@@ -41,3 +57,20 @@ export default class Home extends Component {
   }
 }
 
+const styles = {
+  pageStyle: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#ECF2F4',
+    marginBottom: 20,
+ 
+  },
+  listStyle: {
+    justifyContent: 'space-between',
+    height: 100,
+  },
+  listItem: {
+
+  }
+
+};
