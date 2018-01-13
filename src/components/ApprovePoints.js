9cbeecc92959975c72be698490bb8201d3c60ea0
Yosif Al-Khlefah <435102878@student.ksu.edu.sa>
2018-01-14 01:43:15 +0300

    approvePoints

--- src/components/ApprovePoints.js
+++ src/components/ApprovePoints.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View,  Image  } from 'react-native';
+import { Text, View,  Image, ScrollView  } from 'react-native';
 import { Card, ListItem, Button } from 'react-native-elements';
 
 class ApprovePoints extends Component {
@@ -15,59 +15,59 @@ class ApprovePoints extends Component {
           events: [
             {
               name: 'بينت بول',
-              description: 'رحنا ولعبنا واستانسنا وانبسطنا',
+              description: 'رحنا ولعبنا واستانسنا وانبسرحنا ولعبنا واستانسنا وانبسطنرحنا ولعبنا و يلسطنوانبسطنطنا',
               leader: 'ناصر العواجي',
             },
             {
                 name: 'استضافه المدرج ',
                 description: 'جبنا ياسر العصيفير',
                 leader: 'ناصر العواجي',
-            }
+            },
+            {
+                name: 'اfff ',
+                description: 'جبنا ياسر العصيفير',
+                leader: 'ناصر العواجي',
+            },
+            {
+                name: 'ffdfdf المدرج ',
+                description: 'جبنا ياسر العصيفير',
+                leader: 'ناصر العواجي',
+            },
            ]
       });
     }
 
-
   render() {
     return (
-      <View>
+      <ScrollView >
           {
     this.state.events.map((item, i) => (
 
-        <Card title={item.name} key={i}>
+       <View style={{ marginBottom: i==this.state.events.length-1? 20:0 }} key={i} >
+        <Card   title={item.name} key={i}>
         
-        <Text style={{marginBottom: 10}}>
+        <Text style={{marginBottom: 25, textAlign: 'center'}}>
         {item.description}
         </Text>
-        <Text style={{marginBottom: 10}}>
-        {item.leader}
+        <Text style={{marginBottom: 10,textAlign: 'center'}}>
+        قائد المشروع: {item.leader} 
         </Text>
         <Button 
         backgroundColor='#03A9F4'
          buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
          title='ارصدني'
          rightIcon={{name: 'work'}}
-          />
+         />
 
         </Card>
+        </View>
     ))
   }
-        
-
-
-      </View>
+      </ScrollView>
       );
   }
 
 }
 
 export default ApprovePoints;
-   
-
-const users = [
- {
-    name: 'brynn',
-    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg'
- },
-];
 
