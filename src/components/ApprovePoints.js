81fed7a9f6fc27ea2f6951d9ec8d4eed2ef0daa2
Yosif Al-Khlefah <435102878@student.ksu.edu.sa>
2018-01-14 01:16:49 +0300

    ApprovePoints

--- src/components/ApprovePoints.js
+++ src/components/ApprovePoints.js
@@ -3,23 +3,58 @@ import { Text, View,  Image  } from 'react-native';
 import { Card, ListItem, Button } from 'react-native-elements';
 
 class ApprovePoints extends Component {
+
+    state = { events: [] } ;
+
+    constructor(props) {
+      super(props);
+    }
+  
+      componentDidMount() {
+        this.setState({
+          events: [
+            {
+              name: 'بينت بول',
+              description: 'رحنا ولعبنا واستانسنا وانبسطنا',
+              leader: 'ناصر العواجي',
+            },
+            {
+                name: 'استضافه المدرج ',
+                description: 'جبنا ياسر العصيفير',
+                leader: 'ناصر العواجي',
+            }
+           ]
+      });
+    }
+
+
   render() {
     return (
       <View>
-        <Card
-title='HELLO WORLD'
->
-<Text style={{marginBottom: 10}}>
- The idea with React Native Elements is more about component structure than actual design.
-</Text>
-<Button
- 
- backgroundColor='#03A9F4'
+          {
+    this.state.events.map((item, i) => (
 
- buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
- title='ارصدني'
- rightIcon={{name: 'work'}} />
-</Card>
+        <Card title={item.name} key={i}>
+        
+        <Text style={{marginBottom: 10}}>
+        {item.description}
+        </Text>
+        <Text style={{marginBottom: 10}}>
+        {item.leader}
+        </Text>
+        <Button 
+        backgroundColor='#03A9F4'
+         buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+         title='ارصدني'
+         rightIcon={{name: 'work'}}
+          />
+
+        </Card>
+    ))
+  }
+        
+
+
       </View>
       );
   }
