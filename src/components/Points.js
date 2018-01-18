b3958f4ae5cb2052541f471a335ea608e516839c
Yosif Al-Khlefah <435102878@student.ksu.edu.sa>
2018-01-18 06:04:05 +0300

    points

--- src/components/Points.js
+++ src/components/Points.js
@@ -7,38 +7,9 @@ import BaseURL from '../config';
 
 class Points extends Component {
 
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      index: '0',
-      data: [
-        // {
-        //   name: 'Dark Souls',
-        //   nickname: 'Best Game!',
-        //   points: '9000'
-        // },
-        // {
-        //   name: 'Demon Souls',
-        //   nickname: 'Best Game!',
-        //   points: '8000'
-        // },
-        // {
-        //   name: 'Borderlands',
-        //   nickname: 'Pretty good!',
-        //   points: '1100'
-        // },
-        // {
-        //   name: 'Monster Hunter',
-        //   nickname: 'Don\'t disappoint me please',
-        //   points: '100'
-        // },
-      ]
-    };
+  state = { data: [] };
 
-  }
-
-  
   componentDidMount() {
     this.getInfo();
   }
