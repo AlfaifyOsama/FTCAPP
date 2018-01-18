b763d56517674360bf170196d32c0c27a88a24b8
Yosif Al-Khlefah <435102878@student.ksu.edu.sa>
2018-01-18 14:43:42 +0300

    mm

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,51 +1,76 @@
 import React, { Component } from 'react';
 import { View, AsyncStorage } from 'react-native';
 import { Card, Button, FormLabel, FormInput } from 'react-native-elements';
+import AwesomeAlert from 'react-native-awesome-alerts';
 import axios from 'axios';
 import BaseURL from '../config';
 
 class MyProfile extends Component {
 
 
-  state = { pass1: '', pass2: '' };
+  state = { pass1: '', pass2: '', showLoadingAlert: false, showErrorAlert: false };
 
   onPress = async () => {
     const value1 = this.state.pass1;
     const value2 = this.state.pass2;
-    if (value1 !== value2) {
-      alert('عندك مشكلة ياريس، تأكد انك كتبت شي');
+    if (value1 !== value2 || value1.lenght < 6 || value1 == undefined || value1 == '' ) {
+        this.showErrorAlert();
       return;
     }
-    if(value.lenght < 6) {
-        alert('عندك خطأ، لازم يحتوي الرقم السري على  30 حرف.');
+    this.showLoadingAlert();
 
-
-        return; 
-    }
-    return;
-    // const token = await AsyncStorage.getItem('token');
-    // const instance = axios.create({
-    //   timeout: 5000,
-    //   headers: { 'Authorization': 'Bearer '+  token }
-    //   });
-    //   instance.post(BaseURL + '/users/send', {
-    //     msg: this.state.message
-    //   })
-    //     .then((response) => {
-    //       //console.log(response.data.users);
-    //       if(response.status == 200){
-    //         this.refs.toast.show('تم :)',500);
-    //         this.setState({ message: '' });
-    //       }
- 
-    //     })
-    //     .catch((error) => {
-    //       console.log(error);
-    //       alert('فيه مشكلة، حاول مرة ثانية');
-    //       this.setState({ message: '' });
-    //     });
+    const token = await AsyncStorage.getItem('token');
+    const id = await AsyncStorage.getItem('userID');
+    const instance = axios.create({
+      timeout: 5000,
+      headers: { 'Authorization': 'Bearer '+  token }
+      });
+      instance.put(BaseURL + '/users/update', {
+        password: value1
+      })
+        .then((response) => {
+          //console.log(response.data.users);
+          if(response.status === 200){
+            this.hideLoadingAlert();
+            this.props.navigation.goBack(null);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          this.showErrorAlert();
+          this.setState({ pass1: '', pass2: '' });
+        });
 
   }
+
+  showLoadingAlert = () => {
+    this.setState({
+        showLoadingAlert: true
+    });
+  }
+
+  hideLoadingAlert = () => {
+    this.setState({
+      showLoadingAlert: false
+    });
+  }
+  showErrorAlert = () => {
+    this.setState({
+        showErrorAlert: true
+    });
+  }
+  hideErrorAlert = () => {
+    this.setState({
+        showErrorAlert: false
+    });
+  }
+
+
+  onCancelPressed = () => {
+    this.hideErrorAlert();
+  }
+
+
   render() {
     return (
       <View style={styles.pageStyle} >
@@ -62,6 +87,26 @@ class MyProfile extends Component {
             onPress={() => this.onPress()}
           />
         </Card>
+        <AwesomeAlert
+      show={this.state.showLoadingAlert}
+      showProgress={true}
+      title="Loading"
+      message="لحظات ..."
+      closeOnTouchOutside={true}
+      closeOnHardwareBackPress={false}
+      showCancelButton={false}
+      showConfirmButton={false}
+    />
+    <AwesomeAlert
+      show={this.state.showErrorAlert}
+      title="بروبلم"
+      message="عندك مشلكلة، تأكد ان الخانتين نفس الشي ،وان كلمة السر تتجاوز 6 حروف وارقام"
+      closeOnHardwareBackPress={false}
+      showCancelButton={true}
+      cancelText={'طيب'}
+      cancelButtonColor={'red'}
+      onCancelPressed={() => this.hideErrorAlert()}
+    />
       </View>
     );
   }
