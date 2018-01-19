8834a8424fd53ffed835eaf8cf8419895630d8a6
Basel Alebdi <basel.alebdi@gmail.com>
2018-01-19 13:39:35 +0300

    hh

--- src/components/More.js
+++ src/components/More.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, NavigationActions } from 'react-native';
+import { View } from 'react-native';
 import { List, ListItem } from 'react-native-elements';
 
 
