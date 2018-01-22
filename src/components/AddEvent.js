e721b6e21b75839fc2d859511a8f51a0011cddb5
o-aloqaily <osamaoqaily@gmail.com>
2018-01-22 20:28:21 +0300

    events, home

--- src/components/AddEvent.js
+++ src/components/AddEvent.js
@@ -63,7 +63,6 @@ class AddEvent extends Component {
     timeout: 3000,
     headers: { 'Authorization':  token }
     });
-
     const param = {
       name: projectName,
       description: projectDisc,
@@ -72,8 +71,7 @@ class AddEvent extends Component {
       users: selectedIDs,
       date,
       whatsapp
-    }
-
+    };
     instance.post(BaseURL + '/events/create', param)
       .then((response) => {
         this.setState({ loading: false });
@@ -83,13 +81,11 @@ class AddEvent extends Component {
       .catch((error) => {
         this.setState({ loading: false });
         alert('حصلت مشكلة، تأكد انك دخلت البيانات كاملة وجرب مرة ثانية');
+        console.log(error.response.data);
       });
   }
 
-
   getInfo = async () => {
-    console.log('getInfo');
-
    const token = 'Bearer ' + await AsyncStorage.getItem('token');
 
    const instance = axios.create({
@@ -108,8 +104,6 @@ class AddEvent extends Component {
   }
 
   renderNames(query) {
-    console.log('renderNames');
-
     if (query === '') {
       return [];
     }
@@ -121,8 +115,6 @@ class AddEvent extends Component {
   }
 
   renderSelectedNames = () => {
-    console.log('renderSelectedNames');
-
     if(this.state.selected.length > 0)
       return this.state.selected.map((member, i) => (
         i === 0 ? <Text style={{ color: '#515151' }} key={i}> {member} </Text> : <Text style={{ color: '#515151' }} key={i}> {member}،</Text>
@@ -130,8 +122,6 @@ class AddEvent extends Component {
   }
 
   getNumbersTo60() {
-    console.log('getNumbersTo60');
-
     const numbers = [];
     for (let i = 1; i <= 60; i++) {
       numbers.push({ value: i });
@@ -140,8 +130,6 @@ class AddEvent extends Component {
   }
 
   renderButtonOrSpinner() {
-    console.log('renderButtonOrSpinner');
-
     if (this.state.loading) {
       return <Spinner size={'small'} />;
     }
