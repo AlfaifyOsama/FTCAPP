5973527821e9f754a05ab28930aeaf011a0e9354
Yosif Al-Khlefah <435102878@student.ksu.edu.sa>
2018-01-25 13:30:16 +0300

    kk

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ export default class App extends React.Component {
   async onIds(device) {
     const deviceID = await AsyncStorage.getItem('device_id');
     console.log('Device info: ', deviceID);
-    if (deviceID != undefined && deviceID != '' && deviceID !=null)
+    if (deviceID != undefined && deviceID != '' && deviceID != null)
       return;
     console.log('Device info: ', device);
     const token = await AsyncStorage.getItem('token');
@@ -55,7 +55,7 @@ export default class App extends React.Component {
       device_id: device.userId
     })
       .then((response) => {
-        if (response.status == 200){
+        if (response.status == 200) {
           console.log('finished sending the request');
           AsyncStorage.setItem('device_id', device.userId);
           OneSignal.sendTag('device_id', 'true');
@@ -67,13 +67,9 @@ export default class App extends React.Component {
   }
 
   render() {
-         // Root tabs is the navigator..
+    // Root tabs is the navigator..
     return (
-
-<MainStack />
-
-
-
+      <MainStack />
     );
   }
 }
